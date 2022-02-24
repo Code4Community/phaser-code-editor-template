1c361c3c98e4ef52bf2dd9252d65cca943e35fa9
maxdg99 <maxdg99@gmail.com>
2022-02-24 02:29:58 -0500

    Testing thene functionality

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import Phaser from "phaser";
 import C4C from "c4c-editor-and-interpreter";
 import ExampleScene from "./scenes/ExampleScene";
 
-C4C.Editor.create(document.body, true);
+// You can define a custom theme here and pass it into .create below
+const theme = {
+  "&": {
+    color: "red",
+    backgroundColor: "gray",
+  },
+  ".cm-content, .cm-gutter": {
+    minHeight: "600px",
+  }
+}
+
+C4C.Editor.create(document.body, null, true);
 
 var config = {
   parent: "body",
