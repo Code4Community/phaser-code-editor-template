5193cd75df777b478a63b600aeefe5af328b2c93
Carter Scheatzle <Carter Scheatzle>
2023-10-16 19:48:47 -0400

    Adding court to background and starting to add player sprite.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import Phaser from "phaser";
 import C4C from "c4c-lib";
 import ExampleScene from "./scenes/ExampleScene";
+import Scene1 from "./scenes/Scene1";
 
 // You can define a custom theme here and pass it into .create below
 const theme = {
   "&": {
     color: "red",
     backgroundColor: "gray",
-  },
-  ".cm-content, .cm-gutter": {
-    minHeight: "600px",
   }
+  // ".cm-content, .cm-gutter": {
+  //   minHeight: "600px",
+  // }
 }
 
 C4C.Editor.create(document.body, null, true);
@@ -18,12 +19,12 @@ C4C.Editor.create(document.body, null, true);
 var config = {
   parent: "body",
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 1262,
+  height: 346,
   dom: {
     createContainer: true,
   },
-  scene: [ExampleScene],
+  scene: [Scene1],
 };
 
 const game = new Phaser.Game(config);
--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -2,7 +2,9 @@ import Phaser from "phaser";
 import Physics from "phaser";
 import logoImg from "../assets/logo.png";
 import smileyImg from "../assets/smiley.png";
+import court from "../assets/court.png";
 import C4C from "c4c-lib";
+
 
 function enterButtonHoverState(btn) {
   btn.setStyle({ fill: "#ff0" });
@@ -12,7 +14,7 @@ function enterButtonRestState(btn) {
   btn.setStyle({ fill: "#fff" });
 }
 
-export default class ExampleScene extends Phaser.Scene {
+export default class Scene1 extends Phaser.Scene {
   constructor() {
     super("Example");
   }
@@ -20,73 +22,14 @@ export default class ExampleScene extends Phaser.Scene {
   preload() {
     this.load.image("logo", logoImg);
     this.load.image("smiley", smileyImg);
+    this.load.image('court', court);
+    //this.load.image('playa', 'assets/derrick_rose.png');;
   }
 
   create() {
-    // This is the text for a program which calls the function stored in the
-    // symbol "alert-hello". It starts with a line comment, and then a function
-    // call.
-    C4C.Editor.Window.init(this);
-    C4C.Editor.Window.open();
-    C4C.Editor.setText(`moveRight(20)`);
 
-    this.smiley = this.add.sprite(400, 300, "smiley");
+    this.add.image(0, 0, 'court').setOrigin(0,0);
+    // player = this.physics.add.sprite(100, 450, 'playa');
 
-    // Define new function and store it in the symbol "alert-hello". This
-    // function can now be called from our little language.
-    C4C.Interpreter.define("alertHello", () => {
-      alert("hello");
-    });
-
-    C4C.Interpreter.define("moveRight", (x_dist) => {
-      this.smiley.x += x_dist;
-    });
-
-    C4C.Interpreter.define("moveLeft", (x_dist) => {
-      this.smiley.x -= x_dist;
-    });
-
-    // Create some interface to running the interpreter:
-
-    // Run Button
-    const runButton = this.add
-          .text(550, 100, "Evaluate", { fill: "#fff", fontSize: "30px" })
-          .setInteractive()
-          .on("pointerdown", () => {
-            const programText = C4C.Editor.getText();
-            // HERE'S THE IMPORTANT PART!!
-            C4C.Interpreter.run(programText);
-          })
-          .on("pointerover", () => enterButtonHoverState(runButton))
-          .on("pointerout", () => enterButtonRestState(runButton));
-
-    // Editor Button
-    const editorButton = this.add
-          .text(500, 200, "Toggle Editor", { fill: "#fff", fontSize: "30px" })
-          .setInteractive()
-          .on("pointerdown", () => {
-            C4C.Editor.Window.toggle();
-          })
-          .on("pointerover", () => enterButtonHoverState(editorButton))
-          .on("pointerout", () => enterButtonRestState(editorButton));
-
-    // Check Button
-    const checkButton = this.add
-          .text(550, 300, "Check", { fill: "#fff", fontSize: "30px" })
-          .setInteractive()
-          .on("pointerdown", () => {
-            const programText = C4C.Editor.getText();
-            // HERE'S THE IMPORTANT PART!!
-            
-            try {
-              C4C.Interpreter.check(programText);              
-            } catch (err) {
-              console.log("Caught something: " + err);
-            } finally {
-              console.log("Done handling");
-            }
-          })
-          .on("pointerover", () => enterButtonHoverState(checkButton))
-          .on("pointerout", () => enterButtonRestState(checkButton));
   }
 }
